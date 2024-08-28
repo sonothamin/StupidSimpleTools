23e98fcd322f74a55be38510b0ef827558c79de7
Sonoth Amin <76142440+sonothamin@users.noreply.github.com>
2024-08-29 04:23:50 +0600

    Index.JS Dynamic Routing

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve tool scripts dynamically from /tools directory
-app.get('/tools/:toolName', (req, res) => {
-    const toolName = req.params.toolName;
-    const toolPath = path.join(__dirname, 'tools', `${toolName}.js`);
-
-    fs.access(toolPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).send('Tool not found');
-        }
-        res.setHeader('Content-Type', 'application/javascript');
-        res.sendFile(toolPath);
-    });
-});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -34,6 +21,20 @@ function getToolsList() {
         };
     });
 }
+
+// Serve tool scripts dynamically from /tools directory
+app.get('/tools/:toolName', (req, res) => {
+    const toolName = req.params.toolName;
+    const toolPath = path.join(__dirname, 'tools', `${toolName}.js`);
+
+    fs.access(toolPath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return res.status(404).send('Tool not found');
+        }
+        res.setHeader('Content-Type', 'application/javascript');
+        res.sendFile(toolPath);
+    });
+});
 
 // Route to render the index page with tools
 app.get('/', (req, res) => {
